b1d00ccd699be8814cbcef6c6b36d7c068a8ba01
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-07-29 23:28:25 -0400

    feat: populate CartButton component

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,6 +1,23 @@
+import { BsCart2 } from "react-icons/bs"
+import Link from "next/link"
+import { Button } from "../ui/button"
+
 function CartButton() {
+  const numItemsInCart = "99" // TODO: Make dynamic
   return (
-    <div>CartButton</div>
+    <Button
+      size='icon'
+      variant={"outline"}
+      asChild
+      className='flex justify-center items-center relative'
+    >
+      <Link href='/cart'>
+        <BsCart2 />
+        <span className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center'>
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
   )
 }
 export default CartButton
