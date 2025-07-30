a145d66b4151a80c99ab21dcf938a43ddc18ea16
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-07-29 23:13:53 -0400

    feat: implement Logo component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,6 +1,13 @@
+import { BsBoxSeam } from "react-icons/bs"
+import { Button } from "../ui/button"
+import Link from "next/link"
 function Logo() {
   return (
-    <div>Logo</div>
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <BsBoxSeam className='w-6 h-6' />
+      </Link>
+    </Button>
   )
 }
 export default Logo
