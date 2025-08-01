1bbf173ae6d8e614c43171c066ff602cc8c64edd
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-01 00:37:34 -0400

    feat: implemented LinksDropdown

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,6 +1,40 @@
+import { BsList } from "react-icons/bs"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { links } from "@/utils/links"
+
 function LinksDropdown() {
   return (
-    <div>LinksDropdown</div>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='outline' className='flex gap-4 max-w-[100px]'>
+            <BsList className='w-6 h-6' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-36' align='end' sideOffset={10}>
+          {links.map((link) => {
+            return (
+              <DropdownMenuItem
+                key={link.href}
+                className='flex w-full text-right justify-end'
+              >
+                <Link href={link.href} className='capitalize w-full'>
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            )
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   )
 }
 export default LinksDropdown
