2bf028b1d6a88b2df7cf325ddd7b204f97ac8541
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-07-31 23:20:17 -0400

    feat: implement theme-provider and add it to providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,19 @@
 "use client"
+
+import { ThemeProvider } from "./theme-provider"
+
 function providers({ children }: { children: React.ReactNode }) {
-  return children
+  return (
+    <>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  )
 }
 export default providers
