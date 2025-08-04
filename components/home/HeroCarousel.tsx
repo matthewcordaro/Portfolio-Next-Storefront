4c3a4ad1db4a000118c5be5b4d6440ac9240e24a
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 16:04:16 -0400

    feat: implemented HeroCarousel

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,4 +1,44 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+import hero1 from "@/public/images/1.jpg"
+import hero2 from "@/public/images/2.jpg"
+import hero3 from "@/public/images/3.jpg"
+import hero4 from "@/public/images/4.jpg"
+
+const carouselImages = [hero1, hero2, hero3, hero4]
+
 function HeroCarousel() {
-  return <div>HeroCarousel</div>
+  return (
+    <div className='hidden lg:block'>
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className='p-2'>
+                    <Image
+                      src={image}
+                      alt='hero'
+                      className='w-full h-[24rem] rounded-md object-cover'
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            )
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  )
 }
 export default HeroCarousel
