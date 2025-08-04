25baa3788b87a295ed27411a2d83d40e3b644954
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 17:47:45 -0400

    feat: implement AboutPage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,18 @@
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <section>
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
+        About our
+        <span className='bg-primary py-2 px-4 rounded-lg tracking-widest text-white'>
+          store
+        </span>
+      </h1>
+      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        veniam ipsa iure cum quo velit, adipisci, recusandae quos atque illum
+        quam doloremque eos quas alias, est explicabo facere. Recusandae, nihil.
+      </p>
+    </section>
   )
 }
 export default AboutPage
