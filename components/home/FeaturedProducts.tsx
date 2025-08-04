7f1b16c243bcb7fa101cf2444189c415a91a9847
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 14:46:25 -0400

    feat: implement FeaturedProducts component

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -1,4 +1,16 @@
-function FeaturedProducts() {
-  return <div>FeaturedProducts</div>
+import { fetchFeatureProducts } from "@/utils/actions"
+import SectionTitle from "../global/SectionTitle"
+import EmptyList from "../global/EmptyList"
+import ProductsGrid from "../products/ProductsGrid"
+
+async function FeaturedProducts() {
+  const featuredProducts = await fetchFeatureProducts()
+  if (featuredProducts.length === 0) return <EmptyList />
+  return (
+    <section className='pt-24'>
+      <SectionTitle text='Featured products' />
+      <ProductsGrid products={featuredProducts} />
+    </section>
+  )
 }
 export default FeaturedProducts
