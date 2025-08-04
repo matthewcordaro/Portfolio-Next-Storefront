e53390697a827a67a695e36cd9379098ff956622
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 13:40:35 -0400

    chore: added comments to db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -1,3 +1,16 @@
+/**
+ * Provides a singleton instance of the PrismaClient for database access.
+ *
+ * This utility ensures that only one instance of PrismaClient is created and reused
+ * across the application, especially during development with hot-reloading.
+ *
+ * - In production, a new PrismaClient instance is created per invocation.
+ * - In development, the instance is attached to the global object to prevent
+ *   exhausting database connections due to repeated instantiation.
+ *
+ * @module utils/db
+ * @see {@link https://www.prisma.io/docs/guides/performance-and-optimization/connection-management}
+ */
 import { PrismaClient } from "@prisma/client"
 
 const prismaClientSingleton = () => new PrismaClient()
