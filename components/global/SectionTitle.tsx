4018c4cea071df7aa128e1f072f02b7c4ec1dcab
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 14:43:35 -0400

    feat: implement EmptyList and SectionTitle components

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,4 +1,13 @@
-function SectionTitle() {
-  return <div>SectionTitle</div>
+import { Separator } from "@/components/ui/separator"
+
+function SectionTitle({ text }: { text: string }) {
+  return (
+    <div>
+      <div className='text-3xl medium tracking-wider capitalize mb-8'>
+        {text}
+      </div>
+      <Separator />
+    </div>
+  )
 }
 export default SectionTitle
