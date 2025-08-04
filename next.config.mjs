2dd8104f8ebc6608eb218bbb336f4dfc80c0501f
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 15:03:54 -0400

    feat: add images.pexels.com to remotePatterns for image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/**",
+      },
+    ],
   },
 }
 
