b5980549fdac3a5ef53e331fd49c98f4951cc51b
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 14:50:34 -0400

    feat: implement FavoriteToggleButton

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,4 +1,11 @@
-function FavoriteToggleButton() {
-  return <div>FavoriteToggleButton</div>
+import { BsHeart } from "react-icons/bs"
+import { Button } from "@/components/ui/button"
+
+function FavoriteToggleButton({ productId }: { productId: string }) {
+  return (
+    <Button size={"icon"} variant={"outline"} className='p-2 cursor-pointer'>
+      <BsHeart />
+    </Button>
+  )
 }
 export default FavoriteToggleButton
