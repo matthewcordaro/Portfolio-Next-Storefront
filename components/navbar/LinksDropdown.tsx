7a76493c0db50be40730cb77f9a8cec5445e6709
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 23:06:53 -0400

    feat: implemented dynamic menu for  LinksDropdown component

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -9,6 +9,14 @@ import {
 import Link from "next/link"
 import { Button } from "../ui/button"
 import { links } from "@/utils/links"
+import UserIcon from "./UserIcon"
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs"
+import SignOutLink from "./SignOutLink"
+
+// TODO: Fix the logout bug where the menu doesn't disappear.
+// This will require moving any server-only logic out of UserIcon.tsx and into
+// a server component or a layout-level fetch. Then passing the result down as
+// props.
 
 function LinksDropdown() {
   return (
@@ -17,21 +25,39 @@ function LinksDropdown() {
         <DropdownMenuTrigger asChild>
           <Button variant='outline' className='flex gap-4 max-w-[100px]'>
             <BsThreeDotsVertical className='w-6 h-6' />
+            <UserIcon />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className='w-36' align='end' sideOffset={10}>
-          {links.map((link) => {
-            return (
-              <DropdownMenuItem
-                key={link.href}
-                className='flex w-full text-right justify-end'
-              >
-                <Link href={link.href} className='capitalize w-full'>
-                  {link.label}
-                </Link>
-              </DropdownMenuItem>
-            )
-          })}
+          <SignedOut>
+            <DropdownMenuItem>
+              <SignInButton mode='modal'>
+                <button className='w-full text-left'>Login</button>
+              </SignInButton>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            {links.map((link) => {
+              return (
+                <DropdownMenuItem
+                  key={link.href}
+                  className='flex w-full text-right justify-end'
+                >
+                  <Link href={link.href} className='capitalize w-full'>
+                    {link.label}
+                  </Link>
+                </DropdownMenuItem>
+              )
+            })}
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <SignOutLink />
+            </DropdownMenuItem>
+          </SignedIn>
         </DropdownMenuContent>
       </DropdownMenu>
     </>
