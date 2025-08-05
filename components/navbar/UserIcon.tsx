3ef24b15447c6092436c2a0fa06dea0d7874466c
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 22:37:19 -0400

    feat: implemented UserIcon component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,18 @@
-function UserIcon() {
-  return (
-    <div>UserIcon</div>
-  )
+import { currentUser } from "@clerk/nextjs/server"
+import { BsPerson } from "react-icons/bs"
+
+async function UserIcon() {
+  const user = await currentUser()
+  const profileImage = user?.imageUrl
+  if (profileImage)
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src={profileImage}
+        alt={user.fullName ?? "User avatar"}
+        className='w-6 h-6 rounded-full object-cover'
+      />
+    )
+  return <BsPerson />
 }
 export default UserIcon
