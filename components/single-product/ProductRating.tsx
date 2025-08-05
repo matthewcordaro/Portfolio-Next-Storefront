9bb98af54c65207c098ae5653dce5ec3d785bc37
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 20:59:32 -0400

    feat: add parenthesis to visually separate number of ratings from the rating

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -9,7 +9,7 @@ function ProductRating({ productId }: { productId: string }) {
   const countValue = `(${count}) reviews`
   return (
     <span className={className}>
-      <BsStar className='w-3 h-3' /> {rating} {count}
+      <BsStar className='w-3 h-3' /> {rating} ({count})
     </span>
   )
 }
