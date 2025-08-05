1810a3e7e1d9d3c8cf0972084bc7704243b21403
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 20:46:54 -0400

    feat: expand AddToCart component placeholder

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,4 +1,16 @@
-function ProductRating() {
-  return <div>ProductRating</div>
+import { BsStar } from "react-icons/bs"
+
+function ProductRating({ productId }: { productId: string }) {
+  // TODO: implement dynamic count and rating
+  const rating = 4.2
+  const count = 25
+
+  const className = `flex gap-1 items-center text-md mt-1 mb-4`
+  const countValue = `(${count}) reviews`
+  return (
+    <span className={className}>
+      <BsStar className='w-3 h-3' /> {rating} {count}
+    </span>
+  )
 }
 export default ProductRating
