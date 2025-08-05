56b3aeb3d56433e1f1ad09176f14efcc09a2aa27
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-05 00:10:17 -0400

    feat: implemented admin Sidebar component

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,4 +1,28 @@
+"use client"
+import { adminLinks } from "@/utils/links"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { Button } from "@/components/ui/button"
+
 function Sidebar() {
-  return <div>Sidebar</div>
+  const pathname = usePathname()
+  return (
+    <aside>
+      {adminLinks.map(({ href, label }) => {
+        const isActivePage = pathname === href
+        const variant = isActivePage ? "default" : "ghost"
+        return (
+          <Button
+            key={href}
+            asChild
+            className='w-full mb-2 capitalize font-normal justify-start'
+            variant={variant}
+          >
+            <Link href={href}>{label}</Link>
+          </Button>
+        )
+      })}
+    </aside>
+  )
 }
 export default Sidebar
