44574e757adba063c5fb173556bfd34e4e61109d
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-04 20:22:02 -0400

    feat: add search term to fetchAllProducts db action and update ProductsContainer

--- utils/actions.ts
+++ utils/actions.ts
@@ -11,8 +11,14 @@ export const fetchFeatureProducts = async () => {
   })
 }
 
-export const fetchAllProducts = async () => {
+export const fetchAllProducts = async ({ search = "" }) => {
   return await db.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
+    },
     orderBy: {
       createdAt: "desc",
     },
