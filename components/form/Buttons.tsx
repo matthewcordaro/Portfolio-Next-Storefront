6235dd0b65ec6caaabecb778d74494719d73b0b1
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-10 19:00:35 -0400

    feat: refactor Buttons component into SubmitButton with loading state

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,6 +1,41 @@
-function Buttons() {
+"use client"
+
+import { ReloadIcon } from "@radix-ui/react-icons"
+import { useFormStatus } from "react-dom"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { SignInButton } from "@clerk/nextjs"
+import { BsHeart, BsHeartFill, BsTrash, BsPencilSquare } from "react-icons/bs"
+
+type bsnSize = "default" | "lg" | "sm"
+
+type SubmitButtonProps = {
+  className?: string
+  text?: string
+  size?: bsnSize
+}
+
+function SubmitButton({
+  className = "",
+  text = "submit",
+  size = "lg",
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus()
   return (
-    <div>Buttons</div>
+    <Button
+      type='submit'
+      disabled={pending}
+      className={cn("capitalize", className)}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
+        </>
+      ) : (
+        text
+      )}
+    </Button>
   )
 }
-export default Buttons
+export default SubmitButton
