2e0b725cdba00a136426671be557ad2181e0e5f7
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-10 14:16:20 -0400

    feat: implement CreateProductPage with form input for product name

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,6 +1,34 @@
+import { Button } from "@/components/ui/button"
+import { faker } from "@faker-js/faker"
+import FormInput from "@/components/form/FormInput"
+
+const createProductAction = async (formData: FormData) => {
+  "use server"
+  const name = formData.get("name") as string
+  console.log(name)
+}
+
 function CreateProductPage() {
+  const name = faker.commerce.productName()
+  const company = faker.company.name()
+  const description = faker.lorem.paragraph({ min: 10, max: 12 })
   return (
-    <div>CreateProductPage</div>
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>create product</h1>
+      <div className='border p-8 rounded-md'>
+        <form action={createProductAction}>
+          <FormInput
+            label='product name'
+            name='name'
+            type='text'
+            defaultValue={name}
+          />
+          <Button type='submit' size='lg'>
+            Submit
+          </Button>
+        </form>
+      </div>
+    </section>
   )
 }
 export default CreateProductPage
