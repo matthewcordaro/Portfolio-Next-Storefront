370096f0f359d172c88273ff499e39dddf546ffd
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-10 20:42:01 -0400

    feat: add user authentication and error handling functions in actions

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,21 @@
 "use server"
 import { redirect } from "next/navigation"
 import db from "./db"
+import { currentUser, User } from "@clerk/nextjs/server"
+
+const getAuthUser = async (): Promise<User> => {
+  const user = await currentUser()
+  if (!user) redirect("/")
+  return user
+}
+
+function renderError(error: unknown) {
+  console.log(error)
+  return {
+    message:
+      error instanceof Error ? error.message : "an unknown error occurred",
+  }
+}
 
 export const fetchFeatureProducts = async () => {
   return await db.product.findMany({
