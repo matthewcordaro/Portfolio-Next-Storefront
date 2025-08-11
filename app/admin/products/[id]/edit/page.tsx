d8dfa11564986c053b8f52f42f6f5c4b517be741
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-11 14:19:02 -0400

    feat: implement EditProductPage with product details form for admin users

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,6 +1,56 @@
-function EditProductPage() {
+import { fetchAdminProduct, updateProductAction } from "@/utils/actions"
+import FormContainer from "@/components/form/FormContainer"
+import FormInput from "@/components/form/FormInput"
+import PriceInput from "@/components/form/PriceInput"
+import TextAreaInput from "@/components/form/TextAreaInput"
+import { SubmitButton } from "@/components/form/Buttons"
+import CheckboxInput from "@/components/form/CheckBoxInput"
+
+async function EditProductPage({ params }: { params: { id: string } }) {
+  const { id } = params
+  const product = await fetchAdminProduct(id)
+  const { name, company, description, featured, price } = product
   return (
-    <div>EditProductPage</div>
+    <>
+      <section>
+        <h1 className='text-2xl font-semibold mb-8 capitalize'>
+          update product
+        </h1>
+        <div className='border p-8 rounded-md'>
+          <FormContainer action={updateProductAction}>
+            <div className='grid gap-4 md:grid-cols-2 my-4'>
+              <input type='hidden' />
+              <FormInput
+                type='text'
+                name='name'
+                label='product name'
+                defaultValue={name}
+              />
+              <FormInput
+                type='text'
+                name='company'
+                label='company'
+                defaultValue={company}
+              />
+              <PriceInput defaultValue={price} />
+            </div>
+            <TextAreaInput
+              name='description'
+              labelText='product description'
+              defaultValue={description}
+            />
+            <div>
+              <CheckboxInput
+                name='featured'
+                label='featured'
+                defaultChecked={featured}
+              />
+            </div>
+            <SubmitButton text='update product' className='mt-8' />
+          </FormContainer>
+        </div>
+      </section>
+    </>
   )
 }
 export default EditProductPage
