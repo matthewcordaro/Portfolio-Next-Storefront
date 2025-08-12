bf62d635812a4b062124361adf586fea7ac73666
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-11 21:35:27 -0400

    feat: update SingleProductPage to use product ID consistently and add a ShareButton

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,10 +5,11 @@ import { formatCurrency } from "@/utils/format"
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton"
 import AddToCart from "@/components/single-product/AddToCart"
 import ProductRating from "@/components/single-product/ProductRating"
+import ShareButton from "@/components/single-product/ShareButton"
 
 async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id)
-  const { name, image, company, description } = product
+  const { name, image, company, description, id } = product
   const dollarAmount = formatCurrency(product.price)
   return (
     <section>
@@ -29,15 +30,18 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
         <div>
           <div className='flex gap-x-8 items-center'>
             <h1 className='capitalize text-3xl font-bold'>{name}</h1>
-            <FavoriteToggleButton productId={params.id} />
+            <div className='flex items-center gap-x-2'>
+              <FavoriteToggleButton productId={id} />
+              <ShareButton name={name} productId={id} />
+            </div>
           </div>
-          <ProductRating productId={params.id} />
+          <ProductRating productId={id} />
           <h4 className='text-xl mt-2'>{company}</h4>
           <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
             {dollarAmount}
           </p>
           <p className='mt-6 leading-8 text-muted-foreground'>{description}</p>
-          <AddToCart productId={params.id} />
+          <AddToCart productId={id} />
         </div>
       </div>
     </section>
