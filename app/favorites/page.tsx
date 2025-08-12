92670d00b350458c1168e405471d4576b6959686
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-11 20:20:40 -0400

    feat: implement FavoritesPage to display user favorites

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,6 +1,16 @@
-function FavoritesPage() {
+import SectionTitle from "@/components/global/SectionTitle"
+import ProductsGrid from "@/components/products/ProductsGrid"
+import { fetchUserFavorites } from "@/utils/actions"
+
+async function FavoritesPage() {
+  const favorites = await fetchUserFavorites()
+  if (favorites.length === 0) return <SectionTitle text='no favorites' />
+
   return (
-    <div>FavoritesPage</div>
+    <div>
+      <SectionTitle text='Favorites' />
+      <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
+    </div>
   )
 }
 export default FavoritesPage
