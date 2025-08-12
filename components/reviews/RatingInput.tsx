0df4c3aea6e66025768a1280900b33534263c1a9
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-11 23:18:04 -0400

    feat: implement RatingInput component with star selection functionality

--- components/reviews/RatingInput.tsx
+++ components/reviews/RatingInput.tsx
@@ -1,4 +1,43 @@
-function RatingInput() {
-  return <div>RatingInput</div>
-}
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+function RatingInput({
+  name,
+  labelText,
+}: {
+  name: string
+  labelText?: string
+}) {
+  const stars = Array.from({ length: 5 }, (_, i) =>
+    (i + 1).toString()
+  ).reverse()
+  return (
+    <div className='mb-2 max-w-xs'>
+      <Label htmlFor={name} className='capitalize'>
+        {labelText || name}
+      </Label>
+      <Select defaultValue={stars[0]} name={name} required>
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {stars.map((star) => {
+            return (
+              <SelectItem key={star} value={star}>
+                {star}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
 export default RatingInput
