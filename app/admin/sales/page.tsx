5ab66af3d053037cd70b2a9a2d330385c5e95557
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-19 21:05:11 -0400

    feat: implement SalesPage

--- app/admin/sales/page.tsx
+++ app/admin/sales/page.tsx
@@ -1,6 +1,52 @@
-function SalesPage() {
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { fetchAdminOrders } from "@/utils/actions"
+import { formatCurrency, formatDate } from "@/utils/format"
+
+async function SalesPage() {
+  const orders = await fetchAdminOrders()
   return (
-    <div>SalesPage</div>
+    <div>
+      <Table>
+        <TableCaption>Total Orders: {orders.length}</TableCaption>
+        <TableHeader>
+          <TableHead>Email</TableHead>
+          <TableHead>Date</TableHead>
+          <TableHead># Items</TableHead>
+          <TableHead>Subtotal</TableHead>
+          <TableHead>Tax</TableHead>
+          <TableHead>Shipping</TableHead>
+          <TableHead>Paid</TableHead>
+          <TableHead>Total</TableHead>
+        </TableHeader>
+        <TableBody>
+          {orders.map(
+            ({ id, numItems, orderTotal, tax, shipping, createdAt, email, subTotal, isPaid }) => {
+              return (
+                <TableRow key={id}>
+                  <TableCell>{email}</TableCell>
+                  <TableCell>{formatDate(createdAt)}</TableCell>
+                  <TableCell>{numItems}</TableCell>
+                  <TableCell>{formatCurrency(subTotal)}</TableCell>
+                  <TableCell>{formatCurrency(tax)}</TableCell>
+                  <TableCell>{formatCurrency(shipping)}</TableCell>
+                  <TableCell>{isPaid ? "Y" : "N"}</TableCell>
+                  <TableCell>{formatCurrency(orderTotal)}</TableCell>
+                </TableRow>
+              )
+            }
+          )}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
 export default SalesPage
