fff13f2e5a761f158645bb42ae840d08e865f21b
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-26 13:35:36 -0400

    refactor: integrate delete functionality directly into ManageableReviewCard by replacing DeleteReviewButton with VerifyActionButton

--- components/reviews/ManageableReviewCard.tsx
+++ components/reviews/ManageableReviewCard.tsx
@@ -1,6 +1,9 @@
+"use client"
 import ReviewCard from "./ReviewCard"
 import UpdateReview from "./UpdateReview"
-import DeleteReviewButton from "./DeleteReviewButton"
+import VerifyActionButton from "@/components/global/VerifyActionButton"
+import { BsTrash } from "react-icons/bs"
+import { deleteReviewAction } from "@/utils/actions"
 
 interface ManageableReviewCardProps {
   reviewInfo: {
@@ -18,7 +21,19 @@ function ManageableReviewCard({ reviewInfo }: ManageableReviewCardProps) {
     <ReviewCard reviewInfo={reviewInfo}>
       <div className='flex items-center gap-x-2'>
         <UpdateReview reviewId={id} />
-        <DeleteReviewButton reviewId={id} />
+        <VerifyActionButton
+          type='destructive'
+          buttonIcon={() => (
+            <BsTrash className='h-4 w-4 text-destructive group-hover:text-white transition-colors' />
+          )}
+          buttonClassName='p-2 rounded bg-transparent hover:bg-destructive group flex items-center justify-center shadow-none'
+          dialogTitle='Delete Review'
+          dialogDescription='Are you sure you want to delete this review? This action cannot be undone.'
+          dialogConfirmText='Delete'
+          verificationAction={async () =>
+            await deleteReviewAction({ reviewId: id })
+          }
+        />
       </div>
     </ReviewCard>
   )
