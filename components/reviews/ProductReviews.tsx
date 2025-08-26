cf40bac31259fd10b3feddb9bfdd6971cc3ee199
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-26 13:31:11 -0400

    refactor: streamline ProductReviews to utilize ManageableReviewCard for user-specific review management

--- components/reviews/ProductReviews.tsx
+++ components/reviews/ProductReviews.tsx
@@ -1,8 +1,7 @@
 import { fetchProductReviews } from "@/utils/actions"
 import ReviewCard from "./ReviewCard"
 import SectionTitle from "../global/SectionTitle"
-import DeleteReviewButton from "./DeleteReviewButton"
-import UpdateReview from "./UpdateReview"
+import ManageableReviewCard from "./ManageableReviewCard"
 
 async function ProductReviews({
   productId,
@@ -19,21 +18,18 @@ async function ProductReviews({
       <div className='grid md:grid-cols-2 gap-8 my-8'>
         {reviews.map(
           ({ comment, rating, authorImageUrl, authorName, id, clerkId }) => {
+            // Use author name and image for review display
             const reviewInfo = {
               comment,
               rating,
               image: authorImageUrl,
               name: authorName,
+              id,
             }
-            return (
-              <ReviewCard key={id} reviewInfo={reviewInfo}>
-                {currentUserId && clerkId === currentUserId && (
-                  <div className='flex items-center gap-x-2'>
-                    <UpdateReview reviewId={id} />
-                    <DeleteReviewButton reviewId={id} />
-                  </div>
-                )}
-              </ReviewCard>
+            return currentUserId && clerkId === currentUserId ? (
+              <ManageableReviewCard key={id} reviewInfo={reviewInfo} />
+            ) : (
+              <ReviewCard key={id} reviewInfo={reviewInfo} />
             )
           }
         )}
