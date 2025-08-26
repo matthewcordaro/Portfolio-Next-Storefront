31a3fa070c6b55ddffb0a17a09ac693cb9ae7b5a
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-26 17:07:57 -0400

    feat: implement UpdateReview component to handle review data and display dialog for editing

--- components/reviews/UpdateReview.tsx
+++ components/reviews/UpdateReview.tsx
@@ -1,6 +1,52 @@
 import { BsPencilSquare } from "react-icons/bs"
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog"
+import FormContainer from "@/components/form/FormContainer"
+import RatingInput from "@/components/reviews/RatingInput"
+import TextAreaInput from "@/components/form/TextAreaInput"
+import { updateReviewAction } from "@/utils/actions"
+import { Button } from "@/components/ui/button"
+import { Review } from "@prisma/client"
 
-function UpdateReview({ reviewId }: { reviewId: string }) {
-  return <BsPencilSquare />
+function UpdateReview({ review }: { review: Review }) {
+  const { id: reviewId, rating, comment, productId } = review
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <span style={{ cursor: "pointer" }}>
+          <BsPencilSquare />
+        </span>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Update your review</DialogTitle>
+        </DialogHeader>
+        <FormContainer action={updateReviewAction}>
+          <input type='hidden' name='reviewId' value={reviewId} />
+          <input type='hidden' name='productId' value={productId} />
+          <RatingInput name='rating' />
+          <TextAreaInput
+            name='comment'
+            labelText='feedback'
+            defaultValue={comment || ""}
+          />
+          <DialogFooter className='flex gap-2 mt-4'>
+            <Button type='submit'>Confirm Change</Button>
+            <DialogTrigger asChild>
+              <Button type='button' variant='outline'>
+                Cancel
+              </Button>
+            </DialogTrigger>
+          </DialogFooter>
+        </FormContainer>
+      </DialogContent>
+    </Dialog>
+  )
 }
 export default UpdateReview
