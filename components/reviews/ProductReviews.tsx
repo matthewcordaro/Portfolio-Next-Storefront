175542d637582494940d58c5fa9061eeda1afc6f
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-26 00:40:41 -0400

    feat: add currentUserId prop to ProductReviews for conditional rendering of delete button

--- components/reviews/ProductReviews.tsx
+++ components/reviews/ProductReviews.tsx
@@ -1,23 +1,38 @@
 import { fetchProductReviews } from "@/utils/actions"
 import ReviewCard from "./ReviewCard"
 import SectionTitle from "../global/SectionTitle"
+import DeleteReviewButton from "./DeleteReviewButton"
 
-async function ProductReviews({ productId }: { productId: string }) {
+async function ProductReviews({
+  productId,
+  currentUserId,
+}: {
+  productId: string
+  currentUserId?: string
+}) {
   const reviews = await fetchProductReviews(productId)
 
   return (
     <div className='mt-16'>
       <SectionTitle text='product reviews' />
       <div className='grid md:grid-cols-2 gap-8 my-8'>
-        {reviews.map(({ comment, rating, authorImageUrl, authorName, id }) => {
-          const reviewInfo = {
-            comment,
-            rating,
-            image: authorImageUrl,
-            name: authorName,
+        {reviews.map(
+          ({ comment, rating, authorImageUrl, authorName, id, clerkId }) => {
+            const reviewInfo = {
+              comment,
+              rating,
+              image: authorImageUrl,
+              name: authorName,
+            }
+            return (
+              <ReviewCard key={id} reviewInfo={reviewInfo}>
+                {currentUserId && clerkId === currentUserId && (
+                  <DeleteReviewButton reviewId={id} />
+                )}
+              </ReviewCard>
+            )
           }
-          return <ReviewCard key={id} reviewInfo={reviewInfo} />
-        })}
+        )}
       </div>
     </div>
   )
