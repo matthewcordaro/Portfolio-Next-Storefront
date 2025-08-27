d49603d6252dfcfa0f3083995fa4c5be1effb9e4
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-26 23:41:39 -0400

    feat: display environment mode warning in Navbar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,22 +5,33 @@ import LinksDropdown from "./LinksDropdown"
 import Logo from "./Logo"
 import NavSearch from "./NavSearch"
 import { Suspense } from "react"
+import { nodeEnvironment } from "@/utils/env"
 
 function Navbar() {
   return (
-    <nav className='border-b'>
-      <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4'>
-        <Logo />
-        <Suspense>
-          <NavSearch />
-        </Suspense>
-        <div className='flex gap-4 items-center'>
-          <CartButton />
-          <ThemeDropdownButton />
-          <LinksDropdown />
-        </div>
-      </Container>
-    </nav>
+    <>
+      {nodeEnvironment !== "production" && (
+        <>
+          <div className='text-sm text-yellow-600 font-semibold text-center'>
+            ⚠️ SITE IN {nodeEnvironment.toUpperCase()} MODE ⚠️
+          </div>
+          <hr />
+        </>
+      )}
+      <nav className='border-b'>
+        <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4'>
+          <Logo />
+          <Suspense>
+            <NavSearch />
+          </Suspense>
+          <div className='flex gap-4 items-center'>
+            <CartButton />
+            <ThemeDropdownButton />
+            <LinksDropdown />
+          </div>
+        </Container>
+      </nav>
+    </>
   )
 }
 export default Navbar
