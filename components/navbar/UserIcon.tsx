299257b6830856ba453d73e18f6ce943dc3b4443
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-08-29 00:08:35 -0400

    Refactor UserIcon component into a client side component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,17 +1,18 @@
-import { currentUser } from "@clerk/nextjs/server"
+"use client"
+import { useUser } from "@clerk/nextjs"
 import { BsPerson } from "react-icons/bs"
 
-async function UserIcon() {
-  const user = await currentUser()
+function UserIcon() {
+  const { user } = useUser()
   const profileImage = user?.imageUrl
   if (profileImage)
     return (
-      // eslint-disable-next-line @next/next/no-img-element
-      <img
-        src={profileImage}
-        alt={user.fullName ?? "User avatar"}
-        className='w-6 h-6 rounded-full object-cover'
-      />
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={profileImage}
+          alt={user.fullName ?? "User avatar"}
+          className='w-6 h-6 rounded-full object-cover'
+        />
     )
   return <BsPerson />
 }
